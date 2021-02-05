3c022dc1ba93fa8a3f25a9a44450b7dfde44e29b
Jeppe Thaibert <201805267@post.au.dk>
2021-02-05 15:46:37 +0100

    Much faster drawing now. Screw dataframes :(

--- src/graph/viz_ways.py
+++ src/graph/viz_ways.py
@@ -57,7 +57,7 @@ def plotPoints():
     print("  --> points colored")
 
     # plot the whole data frame
-    axes.scatter(intersect["lon"], intersect["lat"], alpha=0.7, s=2, c=intersect["wayID"], alpha=1)
+    axes.scatter(intersect["lon"], intersect["lat"], alpha=0.7, s=2, c=intersect["wayID"])
 
 
 
@@ -65,11 +65,15 @@ def plotLines():
     print("--> plotting lines")
     # Overall idea: currently in the csv, nodes come in ordered from the "way"s.
     # So just check the wayID, and when it changes we know we hit a new way.
-    lines = pd.DataFrame(data=None, columns=["x1", "y1", "x2", "y2", "color"])
+    
     prev_wayID = None
     prev_x = prev_y = None
 
     print("  --> calculating points")
+    lines = {} 
+
+    print("  " + str(all_roads.shape[0]) + "   roads")
+    lines_index = 0 # Keep i and lines_index separate; if we hit a new way, i is incremented and lines_index stays put
     for i in range(all_roads.shape[0]):
         if all_roads["wayID"][i] != prev_wayID:
             # new way, start over again
@@ -81,19 +85,21 @@ def plotLines():
             curr_x, curr_y = all_roads["lon"][i], all_roads["lat"][i]
             color = hashAndColorfy(all_roads["wayID"][i])
 
-            new_row = {"x1": prev_x, "y1": prev_y, "x2": curr_x, "y2": curr_y, "color": color}
-            lines = lines.append(new_row, ignore_index=True) ## TODO SUPER SLOW
-            
-            prev_x, prev_y = curr_x, curr_y
+            lines[lines_index] = {"x1": prev_x, "y1": prev_y, "x2": curr_x, "y2": curr_y, "color": color}
 
+            prev_x, prev_y = curr_x, curr_y
+            lines_index = lines_index + 1 # Only increase index in output dict if we actually add something
 
     # DRAW!
     print("  --> drawing lines")
-    for i in range(lines.shape[0]):
+    for i in range(0, len(lines)):
         # x is an array of [x1, x2], y an array of [y1, y2]
-        x, y = [lines["x1"][i], lines["x2"][i]], [lines["y1"][i], lines["y2"][i]]
-        plt.plot(x, y, alpha=0.7, linewidth=0.5, c=lines["color"][i]) ##
+        row = lines[i]
+        x1, y1, x2, y2 = row["x1"], row["y1"], row["x2"], row["y2"]
+        color = row["color"]
 
+        xs, ys = [x1, x2], [y1, y2]
+        plt.plot(xs, ys, alpha=0.7, linewidth=0.5, c=color)
 
 
 plotPoints()
