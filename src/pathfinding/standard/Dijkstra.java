013e03512d7755c7d1a4312104c6f8407c2a6b6e
Valkær <au617767@post.au.dk>
2021-02-05 16:01:12 +0100

    pathfinding packages broken atm

--- src/pathfinding/standard/Dijkstra.java
+++ src/pathfinding/standard/Dijkstra.java
@@ -3,5 +3,23 @@ package pathfinding.standard;
 import pathfinding.framework;
 
 public class Dijkstra implements PathfindingAlgo {
-    
+ 
+    /**
+     * Implemented based on the description in the book
+     * 
+     * Talks about directed graph, but does it even matter, it's never going to be better to travel the same edge twice?
+     * I think at least
+    */
+
+    public void solve(G: Graph, w: Weight, s: StartingPoint){
+        
+
+
+
+
+
+        
+    }
+
+
 }
