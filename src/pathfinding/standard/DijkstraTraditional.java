ff94fa840b7c0a9ff8c7c2800dd9845d85fe1931
Jeppe Thaibert <201805267@post.au.dk>
2021-02-18 14:46:27 +0100

    Implemented "traditional" Dijkstra

--- src/pathfinding/standard/DijkstraTraditional.java
+++ src/pathfinding/standard/DijkstraTraditional.java
@@ -6,26 +6,80 @@ import java.util.*;
 
 public class DijkstraTraditional implements PathfindingAlgo {
     private final double INF_DIST = Double.MAX_VALUE;
- 
-    /**
-     * Implemented based on the description in the book
-     * 
-     * Talks about directed graph, but does it even matter, it's never going to be better to travel the same edge twice?
-     * I think at least
-    */
-    private Map<Vertex, Double> dist;
-    private Map<Vertex, Vertex> pred; // S in the algo is pred.keySet()
-    
-    // For visual
-    private List<Edge> edgesConsidered;
 
     public Solution shortestPath(Graph g, Vertex start, Vertex goal){
         System.out.println("--> Running \"traditional\" Dijkstra");
-        return null;
-    }
+        //  Pseudocode from CLRS
+        //  Initialize-Single-Source(G, s) (s = source)
+        //  S = Ø
+        //  Q = G.V
+        //  While Q != Ø
+        //      u = Extract-Min(Q)    
+        //      S = S U {u}
+        //      for each vertex v in G.Adj[u]
+        //          Relax(u,v,w)   
 
-    private boolean relax(Vertex u, Neighbor n) {
-        return false;
+
+
+        Map<Vertex, Double> bestDist = new HashMap<>();
+        Map<Vertex, Vertex> predecessor = new HashMap<>(); 
+        List<Edge> edgesConsidered = new ArrayList<>();
+
+        Comparator<Pair> comp = new DistComparator();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(comp);
+
+        pq.add(new Pair(start, 0));
+
+        g.getAllVertices().stream()
+            .map(v -> new Pair(v, INF_DIST))
+            .forEach(pq::add);
+        
+        int iterations = 0;
+        while (pq.size() > 0) {
+            iterations++;
+
+            if (iterations % 1000 == 0) {
+                System.out.println("  " + iterations);
+            }
+
+            Pair head = pq.poll();
+
+            g.getNeighboursOf(head.v)
+                .forEach(n -> {
+                    // RELAX
+                    edgesConsidered.add(new Edge(head.v, n.v));
+
+                    double maybeNewBestDistance = head.dist + n.distance;
+                    double previousBestDistance = bestDist.getOrDefault(n.v, INF_DIST);
+
+                    if (maybeNewBestDistance < previousBestDistance) {
+                        bestDist.put(n.v, maybeNewBestDistance);
+                        predecessor.put(n.v, head.v);
+
+                        // put back in PQ with new dist, but leave the old, "wrong" dist in there too.
+                        pq.add(new Pair(n.v, maybeNewBestDistance)); 
+                    }
+                });
+        }
+
+
+
+        // Get out the shortest path
+        System.out.println("  --> backtracking solution");
+        List<Vertex> out = new ArrayList<>();
+
+        Vertex temp = goal;
+        while (! start.equals(temp)) {
+            out.add(temp);
+            temp = predecessor.get(temp);
+        }
+        out.add(start);
+        System.out.println("      " + out.size() + " nodes");
+        System.out.println("      " + edgesConsidered.size() + " edges considered");
+
+        Solution solution = new Solution(out, new ArrayList<>()); // edgesConsidered);
+
+        return solution;
     }
 
 
@@ -34,8 +88,8 @@ public class DijkstraTraditional implements PathfindingAlgo {
         Graph graph = GraphPopulator.populateGraph("aarhus-silkeborg-intersections.csv");
 
         // Vertex a = new Vertex(56.1634686,10.1722176); // Viborgvej
-        Vertex a = new Vertex(56.1723636,9.5538336); // Silkeborg
-        Vertex b = new Vertex(56.1828308,10.2037825); // O2/Randersvej
+        Vertex b = new Vertex(56.1723636,9.5538336); // Silkeborg
+        Vertex a = new Vertex(56.1828308,10.2037825); // O2/Randersvej
 
         PathfindingAlgo d = new DijkstraTraditional();
         Solution solution = d.shortestPath(graph, a, b);
@@ -47,19 +101,21 @@ public class DijkstraTraditional implements PathfindingAlgo {
     }
 
 
-    class DistComparator implements Comparator<Vertex> {
-        
-        public long comparisons = 0;
+    class DistComparator implements Comparator<Pair> {
 
         @Override
-        public int compare(Vertex a, Vertex b) {
-            this.comparisons++;
-            return Double.compare(dist.getOrDefault(a, INF_DIST), dist.getOrDefault(b, INF_DIST));
-        }
-
-        public long getComparisons() {
-            return this.comparisons;
+        public int compare(Pair p1, Pair p2) {
+            return Double.compare(p1.dist, p2.dist);
         }
     }
 
+    class Pair {
+        public final Vertex v;
+        public final double dist;
+
+        public Pair(Vertex v, double dist) {
+            this.v = v;
+            this.dist = dist;
+        }
+    }
 }
