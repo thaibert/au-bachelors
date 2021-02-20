0db47682b9e5c48b01e6623338b87e57b8e7c1c4
au617767 <au617767@uni.au.dk>
2021-02-20 21:08:09 +0100

    Implemented bidirectional dijkstra, that seems to work, in the sense that it finds the same route as both A* and normal Dijkstra, but it's slower than both hahaha :)

--- src/pathfinding/standard/BidirectionalDijkstra.java
+++ src/pathfinding/standard/BidirectionalDijkstra.java
@@ -4,37 +4,44 @@ import pathfinding.framework.*;
 import graph.*;
 import java.util.*;
 
-public class BidirectionalDijkstra implements PathfindingAlgo {
+public class BidirectionalDijkstra{
     private final double INF_DIST = Double.MAX_VALUE;
+    double mu = INF_DIST;
 
     //_f means forward algo, _b means backwards algo
-    private Map<Vertex, Double> dist_f;
-    private Map<Vertex, Vertex> pred_f; // S in the algo is pred.keySet()
-    private Map<Vertex, Double> dist_b;
-    private Map<Vertex, Vertex> pred_b; // S in the algo is pred.keySet()
-
+    private Map<Vertex, Double> bestDist_f;
+    private Map<Vertex, Vertex> predecessor_f; // S in the algo is pred.keySet()
+    private Map<Vertex, Double> bestDist_b;
+    private Map<Vertex, Vertex> predecessor_b; // S in the algo is pred.keySet()
+    private Set<Vertex> s_f;
+    private Set<Vertex> s_b;
+    private Vertex bestVertex; 
 
     // For visual
     private List<Edge> edgesConsidered;
     
 
 
-    @Override // TODO problems in taking 2 graphs as input :/
-    public Solution shortestPath(Graph graph, Vertex start, Vertex goal) {
+     // TODO problems in taking 2 graphs as input :/
+    public Solution shortestPath(Graph g, Graph ginv, Vertex start, Vertex goal) {
 
-        dist_f = new HashMap<>();
-        pred_f = new HashMap<>();
-        dist_b = new HashMap<>();
-        pred_b = new HashMap<>();
+        // mu? // https://www.ucl.ac.uk/~ucahmto/math/2020/05/30/bidirectional-dijkstra.html
 
+        bestDist_f = new HashMap<>();
+        predecessor_f = new HashMap<>();
+        bestDist_b = new HashMap<>();
+        predecessor_b = new HashMap<>();
+        s_f = new HashSet<>();
+        s_b = new HashSet<>();
+        
         //Purely for visualising
         edgesConsidered = new ArrayList<>();
 
         // Main source of inspiration:
         // https://www.ucl.ac.uk/~ucahmto/math/2020/05/30/bidirectional-dijkstra.html
         
-        dist_f.put(start, 0.0);
-        dist_b.put(goal, 0.0);
+        bestDist_f.put(start, 0.0);
+        bestDist_b.put(goal, 0.0);
 
 
         // Main algo
@@ -46,31 +53,104 @@ public class BidirectionalDijkstra implements PathfindingAlgo {
         pq_b.add(new Pair(goal, 0));
 
         while (pq_f.size() > 0 && pq_b.size() > 0) {
+
             Pair head_f = pq_f.poll();
             Pair head_b = pq_b.poll();
 
-            
+            s_f.add(head_f.v); 
+            s_b.add(head_b.v);
+
+            // TODO BREAK CONDITION
+            if (bestDist_b.get(head_b.v) + 
+                bestDist_f.get(head_f.v) > mu) {
+                System.out.println("Entered exit");
+                break;
+            }
+
+
+            g.getNeighboursOf(head_f.v)
+                .forEach(n -> {
+                    // RELAX
+                    edgesConsidered.add(new Edge(head_f.v, n.v));
+
+                    double maybeNewBestDistance = head_f.dist + n.distance;
+                    double previousBestDistance = bestDist_f.getOrDefault(n.v, INF_DIST);
+
+                    if (maybeNewBestDistance < previousBestDistance) {
+                        bestDist_f.put(n.v, maybeNewBestDistance);
+                        predecessor_f.put(n.v, head_f.v);
+
+                        // put back in PQ with new dist, but leave the old, "wrong" dist in there too.
+                        pq_f.add(new Pair(n.v, maybeNewBestDistance)); 
+                    }
+
+                    if (s_b.contains(n.v) && bestDist_f.get(head_f.v) + n.distance + bestDist_b.get(n.v) < mu) {
+                        mu = bestDist_f.get(head_f.v) + n.distance + bestDist_b.get(n.v);
+                        bestVertex = n.v;
+                    }
+                });
+           
+            ginv.getNeighboursOf(head_b.v)
+            .forEach(n -> {
+                // RELAX
+                edgesConsidered.add(new Edge(head_b.v, n.v));
+
+                double maybeNewBestDistance = head_b.dist + n.distance;
+                double previousBestDistance = bestDist_b.getOrDefault(n.v, INF_DIST);
+
+                if (maybeNewBestDistance < previousBestDistance) {
+                    bestDist_b.put(n.v, maybeNewBestDistance);
+                    predecessor_b.put(n.v, head_b.v);
+
+                    // put back in PQ with new dist, but leave the old, "wrong" dist in there too.
+                    pq_b.add(new Pair(n.v, maybeNewBestDistance)); 
+                }
+
+                if (s_f.contains(n.v) && bestDist_b.get(head_b.v) + n.distance + bestDist_f.get(n.v) < mu) {
+                    mu = bestDist_f.get(head_b.v) + n.distance + bestDist_f.get(n.v);
+                    bestVertex = n.v;
+                }
+            });
+        
 
 
 
         }
 
-        // TODO Auto-generated method stub
-        return null;
-    }
+        // Get out the shortest path
+        System.out.println("  --> backtracking solution");
+        List<Vertex> out = new ArrayList<>();
 
+        /* TODO something that checks if we actually found something
+        if (predecessor.get(goal) == null) {
+            System.out.println("  --> No path exists!!");
+            return new Solution(new ArrayList<>(), edgesConsidered);
+        }*/
 
-    private boolean relax(Vertex u, Neighbor n, Map<Vertex, Double> dist, Map<Vertex, Vertex> pred) {
-        // for visualising all considered edges
-        edgesConsidered.add(new Edge(u, n.v));
-
-        if (dist.getOrDefault(n.v, INF_DIST) > dist.getOrDefault(u, INF_DIST) + n.distance) {
-            dist.put(n.v, dist.get(u) + n.distance);
-            pred.put(n.v, u);
-
-            return true;
+        Vertex temp = bestVertex;
+        while (! start.equals(temp)) {
+            out.add(temp);
+            temp = predecessor_f.get(temp);
         }
-        return false;
+
+        List<Vertex> out2 = new ArrayList<>();
+        temp = bestVertex;
+        while (! goal.equals(temp)) {
+            temp = predecessor_b.get(temp);
+            out2.add(temp);
+        }
+        out.add(start);
+        Collections.reverse(out);
+        out.addAll(out2);
+        
+        System.out.println("      " + out.size() + " nodes");
+        System.out.println("      " + edgesConsidered.size() + " edges considered");
+        System.out.println("      " + comp.getComparisons() + " comparisons");
+
+        Solution solution = new Solution(out, edgesConsidered); // edgesConsidered);
+
+        return solution;
+
     }
     
     public static void main(String[] args) {
@@ -79,11 +159,11 @@ public class BidirectionalDijkstra implements PathfindingAlgo {
         Graph invertedGraph = GraphPopulator.populateGraph("aarhus-silkeborg-intersections.csv", true);
 
         // Vertex a = new Vertex(56.1634686,10.1722176); // Viborgvej
-        Vertex b = new Vertex(56.1723636,9.5538336); // Silkeborg
-        Vertex a = new Vertex(56.1828308,10.2037825); // O2/Randersvej
+        Vertex a = new Vertex(56.1723636,9.5538336); // Silkeborg
+        Vertex b = new Vertex(56.1828308,10.2037825); // O2/Randersvej
 
         BidirectionalDijkstra d = new BidirectionalDijkstra();
-        Solution solution = d.shortestPath(graph, a, b);
+        Solution solution = d.shortestPath(graph, invertedGraph, a, b);
 
         GraphVisualiser vis = new GraphVisualiser(graph, BoundingBox.AarhusSilkeborg);
         vis.drawPath(solution.getShortestPath());
