b346ad639dc9091fff56981f4bad6352b1f34c34
Jeppe Thaibert <201805267@post.au.dk>
2021-02-25 14:40:45 +0100

    Can convert to UTM32N now :D

--- src/datamanipulation/wgs84-to-utm32n.py
+++ src/datamanipulation/wgs84-to-utm32n.py
@@ -1,15 +1,24 @@
 from pyproj import Proj
 import numpy as np
+import pandas as pd
 from pandas import DataFrame
 
 
 wgs84_to_utm32n = Proj("+proj=utm +zone=32N, +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
-# print("\n\n\n\n")
-lons = (10.1722176, 10.5622452)
-lats = (56.1634686, 57.7262073 )
+
+intersections = pd.read_csv("denmark-intersections.csv")
+lats = intersections["lat"]
+lons = intersections["lon"]
 
 
 UTMx, UTMy = wgs84_to_utm32n(lons, lats)
-print( DataFrame(np.c_[UTMx, UTMy], columns=['UTMx', 'UTMy']) )
+# print( DataFrame(np.c_[UTMx, UTMy], columns=['UTMx', 'UTMy']) )
+
+out = pd.DataFrame({"UTMx": UTMx, "UTMy": UTMy, "wayID": intersections["wayID"], "oneway": intersections["oneway"]})
+# print(out)
+# print(intersections)
+
+print("Saving to csv...")
+out.to_csv("denmark-intersections-utm32.csv", index=False)
 
