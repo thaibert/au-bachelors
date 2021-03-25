b996e7649ff6df90e307b1ad4e6f44ee890406bd
Jeppe Thaibert <201805267@post.au.dk>
2021-03-25 11:52:25 +0100

    Actually make sure Pairs are compared properly uh-oh

--- src/utility/Pair.java
+++ src/utility/Pair.java
@@ -11,4 +11,32 @@ public class Pair {
         this.v = v;
         this.dist = dist;
     }
+
+    /*
+    * I think this is covering all the possibilities, but maybe there's something i missed 
+    */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true; 
+        }
+        if (obj == null) {
+            return false;
+        }
+        final Pair other = (Pair) obj;
+        if ((! v.equals(other.v)) || (dist != other.dist)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37; // I saw on https://stackoverflow.com/questions/2265503/why-do-i-need-to-override-the-equals-and-hashcode-methods-in-java 
+                              // That they used prime in auto generated code, so now we do as well ¯\_(ツ)_/¯
+        int vHash = v.hashCode();
+        int distHash = Double.hashCode(dist);
+
+        return (prime + vHash + distHash); 
+    }
 }
