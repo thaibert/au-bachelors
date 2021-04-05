44beab633417f72d457947d4976b45d87f11c43a
au617767 <au617767@uni.au.dk>
2021-04-05 13:15:39 +0200

    Changed the aarhus-silkeborg bounding box a little. Theres some whitespace, but i was unable to see some landmarks from time to time

--- src/graph/BoundingBox.java
+++ src/graph/BoundingBox.java
@@ -5,7 +5,7 @@ public class BoundingBox {
     public static BoundingBox Aarhus = new BoundingBox(56.1269, 56.1846, 10.1365, 10.2580);
     public static BoundingBox GreaterAarhus = new BoundingBox(56.0953, 56.2296, 10.0393, 10.2974);
     public static BoundingBox Christiansbjerg = new BoundingBox(56.1561, 56.1874, 10.1674, 10.2070);
-    public static BoundingBox AarhusSilkeborg = new BoundingBox(56.0337, 56.2794, 9.4807, 10.259);
+    public static BoundingBox AarhusSilkeborg = new BoundingBox(56.0328000, 56.300000, 9.4003000, 10.3546000);
     public static BoundingBox Denmark = new BoundingBox(54.525, 57.799, 8.010, 12.838);
     public static BoundingBox intersection = new BoundingBox(56.1484, 56.1486, 9.85, 9.86); //This is the one from the meeting to test something
     public static BoundingBox Europa = new BoundingBox(29.735139, 81.47299000000001, -34.492960000000004, 46.75348);
@@ -14,7 +14,6 @@ public class BoundingBox {
     public final double NORTH;
     public final double WEST;
     public final double EAST;
-
 
 
     BoundingBox(double SOUTH, double NORTH, double WEST, double EAST) {
