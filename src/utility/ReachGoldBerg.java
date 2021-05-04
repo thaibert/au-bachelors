84f189a8969719646b35e2135443a4d8ee084b88
au617767 <au617767@uni.au.dk>
2021-05-04 15:39:26 +0200

    WIP: I think the tree growing part is done

--- src/utility/ReachGoldBerg.java
+++ src/utility/ReachGoldBerg.java
@@ -7,26 +7,52 @@ import java.util.*;
 
 
 import graph.*;
+import pathfinding.framework.Edge;
 
 public class ReachGoldBerg {
-    
+    private static Double INF_DIST = Double.MAX_VALUE;
+
+
     public static Map<Vertex, Double> reach(Graph graph, double[] bs) {
         Map<Vertex, Double> inPenalties = new HashMap<>();
         Map<Vertex, Double> outPenalties = new HashMap<>();
+
+        Graph graphPrime = graph;
+
+        Map<Edge, Double> r = new HashMap<>();
+        Map<Vertex, Double> rVertex = new HashMap<>();
 
         for (int i = 0; i < bs.length; i++){
             //Iterative step
 
             // Grow trees 
-            for (Vertex v: graphPrime){
-                t_v = partialTree(v, bs[i]);
+            for (Vertex v: graphPrime.getAllVertices()){
+                Tree tree = partialTree(graphPrime, v, bs[i]);
+                // We do not include v according to Goldberg
+                tree.inner.remove(v);
+                for (Vertex u: tree.inner){
+                    for (Neighbor n: graphPrime.getNeighboursOf(u)){
+                        Double tempR = calcReach(u,n.v, tree);
+                        Edge un = new Edge(v, u, 0.0);
+                        if (r.get(un) < tempR){
+                            r.put(un, tempR);
+                        }
+                    }
+                }
             }
             // Prune
+            
 
             // Penalties
-            for (Vertex v: graphPrime){
-                
+            for (Vertex v: graphPrime.getAllVertices()){
+                //TODO in penalties
+
+
+                for (Neighbor n: graphPrime.getNeighboursOf(v)){
+                    outPenalties.put(v, Math.max(outPenalties.getOrDefault(v, 0.0), r.get(new Edge(v, n.v, 0.0))));
+                }
             }
+            
 
             // Shortcuts
             shortcut(graphPrime, bs[i]); //TODO what graph should this be given
@@ -35,12 +61,148 @@ public class ReachGoldBerg {
 
 
 
-        return null;
+        return rVertex;
     }
 
-    public static tree partialTree(Graph g, double epsilon){ //TODO what format is best for T_x 
+    public static Double calcReach(Vertex v, Vertex u, Tree tree){
+
+        return Math.min(depth(v,u, tree), height(v,u,tree));
+
+    }
+
+    public static double depth(Vertex v, Vertex u, Tree tree){
+        return tree.dist.get(u); // TODO i think it's to the end of the edge, but unsure
+    }
+
+    public static double height(Vertex v, Vertex u, Tree tree){
+        Double h = 0.0;
+        for (Vertex w: tree.leafs){
+            if (tree.paths.get(w).contains(v) && tree.closed.contains(w)) {
+                h = Math.max(h, tree.dist.get(w) - tree.dist.get(v)); // If we take the distance to the beginning node, we don't have to keep track of the length of the edge
+            } else if (tree.paths.get(w).contains(v) && !tree.closed.contains(w)){
+                h = INF_DIST;
+            }
+        }
+
+        return h;
+    }
+
+    public static Tree partialTree(Graph g, Vertex x, double epsilon){
+        // This may be super inefficient to maintain all this, but we find it easier than trying to squeze it all 
+        // into one data structure.
+
+        //TODO Something about perturbation????? wtf even is that 
+
+        Set<Vertex> innerCircle = new HashSet<>();
+        Set<Vertex> outerCircle = new HashSet<>();
+
+        Map<Vertex, Double> bestDist = new HashMap<>();
+        Map<Vertex, Vertex> pred = new HashMap<>();
+
+
+        Map<Vertex, Set<Vertex>> paths = new HashMap<>(); // This may not scale idk
+        Map<Vertex, Double> xprimeDist = new HashMap<>();
+
+        Set<Vertex> leafTprime = new HashSet<>(); 
+        Set<Vertex> leafT = new HashSet<>();
+
+        DistComparator comp = new DistComparator();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(comp);
+
+        Map<Vertex, Double> noOfChildren = new HashMap<>();
+
+        Set<Vertex> closed = new HashSet<>();
+
+        pq.add(new Pair(x, 0));
+        bestDist.put(x, 0.0);
+        xprimeDist.put(x, 0.0);
+
+        paths.put(x, new HashSet<Vertex>());
         
-        return null;
+        double bestxPrimeDist = 0.0;
+        
+        while (pq.size() > 0) {
+
+            boolean trueForAllLeaf = leafTprime.size() > 0 && bestxPrimeDist > 2 * epsilon ; // if there are any, assume it's tru and disprove in for loop
+            
+            /*System.out.println(bestDist);
+            System.out.println(xprimeDist);
+            System.out.println(leafT);
+            System.out.println(leafTprime + "\n");*/
+
+            // Stopping condition as shown in https://www.microsoft.com/en-us/research/wp-content/uploads/2006/01/tr-2005-132.pdf
+            for (Vertex v: leafTprime) {
+                if (!(leafT.contains(v) || xprimeDist.get(v) >= 2 * epsilon)){
+                    trueForAllLeaf = false;
+                    break;
+                }
+            }
+            if (trueForAllLeaf){                
+                //System.out.println("Break 1");
+                /*System.out.println("Start: " + start);
+                System.out.println(leafT);
+                System.out.println(leafTprime + "\n");*/
+                break;
+            }
+
+
+            Pair head = pq.poll();
+            if (closed.contains(head.v)){
+                continue;
+            }
+            closed.add(head.v);
+            // Maintain inner and outercircles. 
+            if (xprimeDist.get(head.v) < epsilon) {
+                innerCircle.add(head.v);
+            } else {
+                outerCircle.add(head.v);
+            }
+
+            g.getNeighboursOf(head.v)
+                .forEach(n -> {
+                    // RELAX
+                    if (closed.contains(n.v)){
+                        return;
+                    }
+
+                    double maybeNewBestDistance = head.dist + n.distance;
+                    double previousBestDistance = bestDist.getOrDefault(n.v, INF_DIST);
+
+                    if (maybeNewBestDistance < previousBestDistance) {
+                        leafT.add(n.v);
+                        leafT.remove(head.v);
+                        
+                        Set<Vertex> path = new HashSet<Vertex>(paths.get(head.v));
+                        path.add(head.v);
+                        paths.put(n.v, path); // This should maintain all shortest paths
+
+                        if (!head.v.equals(x)) {
+                            xprimeDist.put(n.v, xprimeDist.get(head.v) + n.distance);
+                        } else {
+                            xprimeDist.put(n.v, xprimeDist.get(head.v) + 0.0);
+                        }
+                        bestDist.put(n.v, maybeNewBestDistance);
+                        Vertex oldParent = pred.get(n.v);
+                        if (oldParent != null){
+                            noOfChildren.put(oldParent, noOfChildren.get(oldParent) -1); // Should never give 0
+                            if (noOfChildren.get(oldParent) == 0){
+                                leafT.add(oldParent);
+                            }
+                        } 
+                        bestDist.put(n.v, maybeNewBestDistance);
+
+                        pred.put(n.v, head.v);
+                        noOfChildren.put(head.v, noOfChildren.getOrDefault(head.v, 0.0) + 1);
+
+                        // put back in PQ with new dist, but leave the old, "wrong" dist in there too.
+                        pq.add(new Pair(n.v, maybeNewBestDistance)); 
+                    }
+                });
+        }
+        //System.out.println("Dijkstra done");
+        Tree tree = new Tree(bestDist, innerCircle, outerCircle, pred, leafT, paths, closed);
+        return tree;
+
     }
 
     public static Graph shortcut(Graph g, double epsilon){
@@ -173,3 +335,23 @@ public class ReachGoldBerg {
 
 }
 
+class Tree {
+    public Map<Vertex, Double> dist;
+    public Set<Vertex> inner;
+    public Set<Vertex> outer;
+    public Map<Vertex,Vertex> pred;
+    public Set<Vertex> leafs;
+    public Map<Vertex, Set<Vertex>> paths;
+    public Set<Vertex> closed;
+
+    public Tree(Map<Vertex, Double> dist, Set<Vertex> inner, Set<Vertex> outer, Map<Vertex,Vertex> pred, Set<Vertex> leafs, Map<Vertex, Set<Vertex>> paths, Set<Vertex> closed){
+        this.dist = dist;
+        this.inner = inner;
+        this.outer = outer;
+        this.pred = pred;
+        this.leafs = leafs;
+        this.paths = paths;
+        this.closed = closed;
+    }
+
+} 
