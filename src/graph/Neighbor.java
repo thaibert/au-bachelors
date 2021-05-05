15df1d71e88de3e7134df34d12f579e13fb3b96e
au617767 <au617767@uni.au.dk>
2021-05-05 11:51:27 +0200

    added equals method to this, so i can use it in ReachGoldberg

--- src/graph/Neighbor.java
+++ src/graph/Neighbor.java
@@ -12,4 +12,30 @@ public class Neighbor {
     public String toString() {
         return v.toString() + "," + distance;
     }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true; 
+        }
+        if (obj == null) {
+            return false;
+        }
+        final Neighbor other = (Neighbor) obj;
+        if ((v != other.v) || (distance != other.distance)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37; // I saw on https://stackoverflow.com/questions/2265503/why-do-i-need-to-override-the-equals-and-hashcode-methods-in-java 
+                              // That they used prime in auto generated code, so now we do as well ¯\_(ツ)_/¯
+        int longHash = Double.hashCode(v.getLatitude());
+        int latHash = Double.hashCode(distance);
+
+        return (prime + longHash + latHash); 
+    }
+
+
 }
