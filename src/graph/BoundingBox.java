c8a1a857805ecffe1dd3032c8ba6eeb04d034c2b
Jeppe Thaibert <201805267@post.au.dk>
2021-05-20 14:30:21 +0200

    Actually use Mercator projection now (aka pretty maps) Also added bounding box for Iceland. May need to use this for reach testing, as pruned Iceland is ~2x pruned aarhus-silkeborg

--- src/graph/BoundingBox.java
+++ src/graph/BoundingBox.java
@@ -9,7 +9,7 @@ public class BoundingBox {
     public static BoundingBox Denmark = new BoundingBox(54.525, 57.799, 8.010, 12.838);
     public static BoundingBox intersection = new BoundingBox(56.1484, 56.1486, 9.85, 9.86); //This is the one from the meeting to test something
     public static BoundingBox Europa = new BoundingBox(29.735139, 81.47299000000001, -34.492960000000004, 46.75348);
-
+    public static BoundingBox Iceland = new BoundingBox(62.845530000000004, 67.50085, -25.740850000000002, -12.41708);
     public static BoundingBox ReachTest = new BoundingBox(56.1474, 56.1497, 9.8977, 9.9194);
 
     public final double SOUTH;
